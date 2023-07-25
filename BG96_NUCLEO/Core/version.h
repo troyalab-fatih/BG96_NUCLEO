47c8a50d129a8384ed93fb440f177c4e2959fd56
Troyalab Fatih <furkan.troyalab@outlook.com>
2023-07-25 23:32:47 +0300

    v0.0.1
    /**************************************************************
     *   Version    : 0.0.1
     *   Tarih      : 11.07.2023
     *   Author     : TroyaLAB Fatih
     *
     *   StmCubeIDE : 1.13.0
     *
     *
     *   Versiyon aciklamalar
     *   ------------------------------
     *  - Proje baslangici
     *  - ioc dosyasında UART1 ayaga kaldirildi.
     *  - Proje git'e yuklendi.
     **************************************************************/

---
+++ BG96_NUCLEO/Core/version.h
@@ -0,0 +1,43 @@
+#ifndef VERSION_H_
+#define VERSION_H_
+
+
+
+/************************************************************
+ *                                                          *
+ *                                                          *
+ *					        BG96                            *
+ *                                                          *
+ *                        TROYALAB                          *
+ *                                                          *
+ *				           NUCLEO                           *
+ *                                                          *
+ *                                                          *
+ ************************************************************/
+
+
+#define 	VERSION  0.0.1
+
+
+#define 	BUILD    1
+#define 	MINOR    0
+#define     MAJOR    0
+
+/**************************************************************
+ *   Version    : 0.0.1
+ *   Tarih      : 11.07.2023
+ *   Author     : TroyaLAB Fatih
+ *
+ *   StmCubeIDE : 1.13.0
+ *
+ *
+ *
+ *   Versiyon aciklamalar
+ *   ------------------------------
+ *  - Proje baslangici
+ *  - ioc dosyasında UART1 ayaga kaldirildi.
+ *  - Proje git'e yuklendi.
+ **************************************************************/
+
+
+#endif /* VERSION_H_ */
